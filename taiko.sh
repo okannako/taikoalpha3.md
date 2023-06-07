73f80f27aeab7596c0ea350e5a0f4f68711c635f
NakoTurk <filmoloji89@gmail.com>
2023-06-08 02:07:25 +0300

    Update taiko.sh

--- taiko.sh
+++ taiko.sh
@@ -24,6 +24,11 @@ sleep 1
 
 echo -e "\e[1m\e[32m>>>Docker Kuruluyor<<< \e[0m" && sleep 2
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.9.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+sudo chown $USER /var/run/docker.sock
+sudo chmod -R 777 /usr/local/bin/docker-compose
 
 echo -e "\e[1m\e[32m>>>Taiko Node Kuruluyor<<< \e[0m" && sleep 2
 git clone https://github.com/taikoxyz/simple-taiko-node.git
